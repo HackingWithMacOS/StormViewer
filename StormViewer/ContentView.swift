5b93b98c0b5aa374d8be8680b2a50422a9e9b913
Chris Hand <cj.hand@gmail.com>
2022-09-11 11:50:23 -0700

    Add assets, make a resizable window

--- StormViewer/ContentView.swift
+++ StormViewer/ContentView.swift
@@ -8,9 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let names = ["Chris", "Eiko", "Takashi", "Alex", "Korina"]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HSplitView {
+            List(names, id:\.self) { name in
+                Text(name)
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            Text("Right")
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+        }
     }
 }
 
