d57684b90415b84296ce6695930971c2cd5a16a8
Chris Hand <cj.hand@gmail.com>
2022-09-11 07:44:49 -0700

    Initial Commit

---
+++ StormViewer/StormViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StormViewerApp.swift
+//  StormViewer
+//
+//  Created by Chris Hand on 9/11/22.
+//
+
+import SwiftUI
+
+@main
+struct StormViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
